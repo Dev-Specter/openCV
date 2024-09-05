21a20e61e9e738e431db9647d1dde874b07ba89c
Abhinav Singh <abhinav.singh16122003@gmail.com>
2024-09-06 00:08:45 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+
+img = cv.imread('/Users/abhinavsingh/Desktop/Opencv/photos/eyes.jpg')
+cv.imshow('Cats', img)
+
+cv.waitKey(0)
+
+# Reading Videos
+capture = cv.VideoCapture('/Users/abhinavsingh/Desktop/Opencv/Videos/10058358-hd_1080_1920_30fps.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    
+    # if cv.waitKey(20) & 0xFF==ord('d'):
+    # This is the preferred way - if `isTrue` is false (the frame could 
+    # not be read, or we're at the end of the video), we immediately
+    # break from the loop. 
+    if isTrue:    
+        cv.imshow('Video', frame)
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break            
+    else:
+        break
+
+capture.release()
+cv.destroyAllWindows()
